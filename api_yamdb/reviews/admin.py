e1c7eb2777bda4ad33eb856d7554c8945855f9c1
Аделина Тазиева <adelina1231@yandex.ru>
2023-04-12 11:13:05 +0300

    models: User, Category, Genre, Title, GenreTitle

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+
+from .models import User, Category, Genre, Title, GenreTitle
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'email',
+        'role',
+        'bio'
+    )
+    search_fields = ('username',)
+    empty_value_display = '-пусто-'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'description',
+        'category'
+    )
+    search_fields = ('name', 'year', 'category')
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class GenreTitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'genre',
+    )
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(GenreTitle, GenreTitleAdmin)
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,139 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    ROLES = (
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin')
+    )
+
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=20,
+        null=True,
+        unique=True
+    )
+    email = models.EmailField(
+        'E-mail',
+        unique=True
+    )
+    role = models.CharField(
+        'Роль',
+        max_length=50,
+        choices=ROLES,
+        default=USER
+    )
+    bio = models.TextField(
+        'Биография',
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Category(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=50
+    )
+    slug = models.SlugField(
+        'Адрес',
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=50
+    )
+    slug = models.SlugField(
+        'Адрес',
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ('name',)
+
+
+class Title(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=100
+    )
+    year = models.IntegerField(
+        'Год выпуска',
+    )
+    description = models.TextField(
+        'Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        through='GenreTitle'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ('name',)
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE)
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}, жанр - {self.genre}'
+
+    class Meta:
+        verbose_name = 'Произведение и жанр'
+        verbose_name_plural = 'Произведения и жанры'
