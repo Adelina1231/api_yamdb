21eef0d636f7114880cc71a606dcc3090e510296
Аделина Тазиева <adelina1231@yandex.ru>
2023-04-23 12:53:15 +0300

    second review

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -2,10 +2,8 @@ from users.models import User
 
 
 def check_email_exist(email):
-    user = User.objects.filter(email=email).first()
-    return user is not None
+    return User.objects.filter(email=email).exists()
 
 
 def check_username_exist(username):
-    user = User.objects.filter(username=username).first()
-    return user is not None
+    return User.objects.filter(username=username).exists()
